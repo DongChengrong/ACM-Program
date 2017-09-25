2e506dbfc1a1f3374450b78891dcc50b39740999
DongChengrong <qazxswh111@163.com>
2017-09-25 16:43:57 +0800

    一次提交

---
+++ LeetCode/9/9_2.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        return str(x) == str(x)[::-1]
