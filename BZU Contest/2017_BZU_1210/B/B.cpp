50343c1b2ddf5bf37cbd2e85394a0798d3ac7d88
DongChengrong <qazxswh111@163.com>
2017-12-10 19:37:13 +0800

    commit

--- BZU Contest/2017_BZU_1210/B/B.cpp
+++ BZU Contest/2017_BZU_1210/B/B.cpp
@@ -13,12 +13,20 @@ int main()
     double k;
     while(~scanf("%d%lf",&n,&k))
     {
-        int i;
-        for(i = 1; i <= n; i++)
-            if((int)(i * k * 0.9 + 0.5) < floor((int)(i * k * 0.9)))
-                break;
+        int ans = 0, tmp = 0;
 
-        printf("%d\n",i);
+        for(int i = 1; i <= n; i++)
+        {
+            tmp += k;
+            int d = tmp % 10;
+            if(d == 6 || d == 7 || d == 8 || d == 9)
+            {
+                ans += (int)(tmp * 0.9 + 0.5);
+                tmp = 0;
+            }
+        }
+        ans += (int)(tmp * 0.9 + 0.5);
+        printf("%d\n",ans);
     }
 	return 0;
 }
