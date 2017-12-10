69510c8a2ba76c2aebdd1493e5813438b1539dd9
DongChengrong <qazxswh111@163.com>
2017-12-10 17:37:21 +0800

    commit

--- BZU Contest/2017_BZU_1210/B/B.cpp
+++ BZU Contest/2017_BZU_1210/B/B.cpp
@@ -1,24 +1,24 @@
 #include<stdio.h>
+#include<math.h>
 #include<string.h>
 #include<algorithm>
 using namespace std;
 
 const int INF = 0x3f3f3f3f;
 const int maxn = 10 + 10;
-int dp[maxn];
 
 int main()
 {
-    int n,k;
-    while(~scanf("%d%d",&n,&k))
+    int n;
+    double k;
+    while(~scanf("%d%lf",&n,&k))
     {
-        memset(dp,0,sizeof(dp));
-        dp[1] = (int)((double) k * 0.9 + 0.5);
+        int i;
+        for(i = 1; i <= n; i++)
+            if((int)(i * k * 0.9 + 0.5) < floor((int)(i * k * 0.9)))
+                break;
 
-        for(int i = 2; i <= n; i++)
-            dp[i] = min(dp[i - 1] + dp[1], (int)((double) k * 0.9 * i + 0.5));
-
-        printf("%d\n",dp[n]);
+        printf("%d\n",i);
     }
 	return 0;
 }
