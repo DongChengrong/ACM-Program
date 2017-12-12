1aafb883b36bd6866b7af29020091ef84cee1d56
DongChengrong <qazxswh111@163.com>
2017-12-12 22:05:26 +0800

    commit
    莫名AC

--- BZU Contest/2017_BZU_数论专题 II/E/e.cpp
+++ BZU Contest/2017_BZU_数论专题 II/E/e.cpp
@@ -4,17 +4,16 @@
 using namespace std;
 
 const int Mod = 1000;
-const int M = 1000;
 const int INF = 0x3f3f3f3f;
 typedef long long LL;
 
 //求后三位
-int pow_tail(int a, int b)
+LL pow_tail(LL a, LL b)
 {
     if(b == 0) return 1;
     if(b == 1) return a % Mod;
 
-    int c = pow_tail(a, b / 2);
+    LL c = pow_tail(a, b / 2);
     c = c * c % Mod;
     if(b & 1) c = c * a % Mod;
     return c;
@@ -28,9 +27,9 @@ double pow_head(double a, LL b)
 
     double c = pow_head(a, b / 2);
     c = c * c;
-    while(c >= M) c /= 10;
+    while(c >= Mod) c /= 10.0;
     if(b & 1) c = c * a;
-    while(c >= M) c /= 10;
+    while(c >= Mod) c /= 10.0;
     return c;
 }
 
@@ -41,14 +40,14 @@ int main()
     scanf("%d",&T);
     for(int kase = 1; kase <= T; kase++)
     {
-        int n,k;
-        scanf("%d%d",&n,&k);
-        int b;
+        LL n,k;
+        scanf("%lld%lld",&n,&k);
+        LL b;
         double a;
         a = pow_head((double)n,k);
         b = pow_tail(n,k);
-        while(a >= 1000) a /= 10;
-        printf("Case %d: %d %d\n",kase,(int)a,b % Mod);
+        while(a >= Mod) a /= 10;
+        printf("Case %d: %03d %03lld\n",kase,(int)a,b%Mod);
     }
 	return 0;
 }
