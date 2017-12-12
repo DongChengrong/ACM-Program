4f06b00a5e6b43553bafc473be4890ec1d4a7d99
DongChengrong <qazxswh111@163.com>
2017-12-12 21:18:18 +0800

    commit

---
+++ BZU Contest/2017_BZU_数论专题 II/E/e.cpp
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<string.h>
+#include<algorithm>
+using namespace std;
+
+const int Mod = 1000;
+const int M = 100000000;
+const int INF = 0x3f3f3f3f;
+typedef long long LL;
+
+//求后三位
+LL pow_tail(LL a, LL b)
+{
+    if(b == 0) return 1;
+    if(b == 1) return a % Mod;
+
+    LL c = pow_tail(a, b / 2);
+    c = c * c % Mod;
+    if(b & 1) c = c * a % Mod;
+    return c;
+}
+
+//求前三位
+LL pow_head(LL a, LL b)
+{
+    if(b == 0) return 1;
+    if(b == 1) return a;
+
+    LL c = pow_tail(a, b / 2);
+    c = c * c;
+    while(c >= M) c /= 10;
+    if(b & 1) c = c * a;
+    while(c >= M) c /= 10;
+    return c;
+}
+
+//主函数
+int main()
+{
+    int T;
+    scanf("%d",&T);
+    for(int kase = 1; kase <= T; kase++)
+    {
+        LL n,k;
+        scanf("%lld%lld",&n,&k);
+        LL a,b;
+        a = pow_head(n,k);
+        b = pow_tail(n,k);
+        while(a >= 1000) a /= 10;
+        printf("Case %d: %lld %lld\n",kase,a,b);
+    }
+	return 0;
+}
