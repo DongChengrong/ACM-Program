9ba741287b49cd6e6c5d6bac9696668f2797bb7a
DongChengrong <qazxswh111@163.com>
2017-12-15 22:47:32 +0800

    ac

--- BZU Contest/2017_BZU_数论专题 II/G/G.cpp
+++ BZU Contest/2017_BZU_数论专题 II/G/G.cpp
@@ -7,34 +7,8 @@ using namespace std;
 
 typedef long long LL;
 
-const double C = 0.57721566490153286060651209;
 const int INF = 0x3f3f3f3f;
 const int maxn = 100000 + 10;
-
-int cnt;
-LL a[maxn];
-
-long long H( LL n ) {
-
-    a[0] = 0;
-    cnt = 1;
-    LL pre = 0;
-    for( LL i = 1; i <= sqrt(n); i++ )
-        if(n / i != pre)
-        {
-            a[cnt] = i;
-            pre = a[cnt++];
-        }
-
-
-    a[cnt] = n;
-    long long res = 0;
-    for( LL i = 1; i <= cnt; i++){
-        res = res + ((n / a[i]) * (a[i] - a[i - 1]));
-        printf("%d\n",a[i]);
-    }
-    return res;
-}
 
 int main()
 {
@@ -42,9 +16,17 @@ int main()
     scanf("%d",&T);
     for(int kase = 1; kase <= T; kase++)
     {
-        LL n,res;
+        LL n,res = 0,i;
         scanf("%lld",&n);
-        res = (log(n) + C) * n;
+
+        for(i = 1; i <= sqrt(n); i++)
+        {
+            res += n / i;
+            if(n / i > n / (i + 1)) res += ((n / i) - (n /  (i + 1))) * i;
+        }
+        i--;
+        if(n / i == i) res -= i;
+
         printf("Case %d: %lld\n",kase,res);
     }
     return 0;
